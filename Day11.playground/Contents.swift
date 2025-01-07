1d624bad364602389d1067367de0c5d285b53a57
Trust-Worthy <jondeveloper0@gmail.com>
2025-01-06 18:27:35 -0700

    Learned about static variables and methods within functions and when to use them.

--- Day11.playground/Contents.swift
+++ Day11.playground/Contents.swift
@@ -7,7 +7,7 @@ import Cocoa
  */
 
 struct BankAccount {
-    var funds = 0
+    private(set) var funds = 0
     
     mutating func deposit(amount:Int) {
         funds += amount
@@ -23,6 +23,9 @@ struct BankAccount {
     }
 }
 // Testing out the struct functionality
+// How do we prevent account.funds -= 1000... by making funds private. This is an access control.
+// properties & methods are dictated by the access control: 
+// private, fileprivate, public, private(set) "let anyone read, but only let my funcs modify"
 
 var account = BankAccount()
 
@@ -33,3 +36,45 @@ if success {
 } else {
     print("Failed to get the money")
 }
+
+/// Static Properties and methods
+/// Sometimes you want to add a property or method to the struct itself (not to an instance)
+/// 1. Example data
+/// 2. Store constant data that has to be accessed in various places of program
+
+
+struct School {
+    /// studentCount and add func belong to the School struct itself not to individual instances.
+    /// One instance across all instances
+    static var studentCount = 0
+    
+    static func add(student: String) {
+        print("\(student) joined the school.")
+        studentCount += 1 /// No mutating because this is a static function.
+    }
+}
+
+School.add(student: "Taylor Swift") /// That's trippy. Adding data directly to the struct without an instance
+print(School.studentCount) /// But it kinda makes sense because this is a school and you are adding students to the School struct
+
+// 1. Acess non-static from static stuff... You can't. Doesn't make sense
+// 2. Acess static stuff from a non-static instance. It's almost like the static makes it globally accessible from all instances.
+// Self.studentCount --> current type of struct or School.studentCount
+/// Self: current type of struct vs self: current values of struct
+
+struct AppData {
+    /// This fixed data below makes a lot of sense to make static
+    static let version = "1.3 beta 2"
+    static let saveFilename = "settings.json"
+    static let homeURL = "https://myserv.com"
+}
+
+struct Employee {
+    let username: String
+    let password: String
+    
+    /// This is a use of a static field for displaying the data when working in swiftui
+    /// Sometimes you want to SEE what you're coding.
+    static let example = Employee(username: "useruseruser", password: "123412342")
+}
+
