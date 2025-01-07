ca71b86e611568579efb50f4c73729a702a31c49
Trust-Worthy <jondeveloper0@gmail.com>
2025-01-06 17:33:05 -0700

    Working on first section of lesson.

--- Day11.playground/Contents.swift
+++ Day11.playground/Contents.swift
@@ -1,3 +1,35 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+/**
+ * @Author: Trust-Worthy
+ * 100 Days of SwiftUI Day 11
+ * Topic: Structs Part 2 & Access Controls
+ */
+
+struct BankAccount {
+    var funds = 0
+    
+    mutating func deposit(amount:Int) {
+        funds += amount
+    }
+    
+    mutating func withdraw(amount:Int) -> Bool{
+        if funds > amount {
+            funds -= amount
+            return true
+        } else {
+            return false
+        }
+    }
+}
+// Testing out the struct functionality
+
+var account = BankAccount()
+
+let success = account.withdraw(amount: 200)
+
+if success {
+    print("Withdrew money successfully")
+} else {
+    print("Failed to get the money")
+}
