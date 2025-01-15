adb870d77f443b31e7a92990c9e3a92f2b884f06
Trust-Worthy <jondeveloper0@gmail.com>
2025-01-14 21:22:22 -0500

    Resetting the code

--- Project-1-WeSplit/WeSplit/WeSplit/ContentView.swift
+++ Project-1-WeSplit/WeSplit/WeSplit/ContentView.swift
@@ -8,49 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
-//    var body: some View {
-//        NavigationStack {
-//            Form {
-//                Section {
-//                    Text("Hello, world!")
-//                    Text("Hello, world!")
-//                    Text("Hello, world!")
-//                }
-//                
-//                Section {
-//                    Text("Hello, world!")
-//                    Text("Hello, world!")
-//                    Text("Hello, world!")
-//                }
-//                
-//                
-//                Text("Hello, world!")
-//                
-//            }
-//            .navigationTitle("SwiftUI")
-//            .navigationBarTitleDisplayMode(.inline)
-//        }
-//        
-//    }
-    /// Swift UI wants to know how to read & write to the name var
-    @State private var selectedStudent = "Harry"
-    let students = ["Jon","Jeff","Jimmy","Harry"]
-    
-    
+
     var body: some View {
-        NavigationStack {
-            Form {
-                Picker("Select your student,", selection: $selectedStudent) {
-                    ForEach(students, id: \.self) {
-                        // id: \.self --> this is used to identify every view uniquely
-                        // The strings themselves are their own unique identifier
-                        // But if there were duplicate strings there would be issues
-                        Text($0)
-                    }
-                }
-            }
-            .navigationTitle("Select a Student")
-        }
+        Text("Hello, Mom & Dad!")
         
     }
 }
