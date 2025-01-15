099617fea82f6865b634d982fc955e92964637cc
Trust-Worthy <jondeveloper0@gmail.com>
2025-01-14 20:17:49 -0500

    Re-add WeSplit as a regular directory

---
+++ Project-1-WeSplit/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Jonathan Bateman on 1/14/25.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
