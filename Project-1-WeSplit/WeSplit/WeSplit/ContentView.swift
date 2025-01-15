899efe83c9a06f4953185d769ef47e0bb8b236e2
Trust-Worthy <jondeveloper0@gmail.com>
2025-01-14 21:08:44 -0500

    Just learned about two way bindings in swift! Now I know how to take input from the user and display it.

--- Project-1-WeSplit/WeSplit/WeSplit/ContentView.swift
+++ Project-1-WeSplit/WeSplit/WeSplit/ContentView.swift
@@ -8,29 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+//    var body: some View {
+//        NavigationStack {
+//            Form {
+//                Section {
+//                    Text("Hello, world!")
+//                    Text("Hello, world!")
+//                    Text("Hello, world!")
+//                }
+//                
+//                Section {
+//                    Text("Hello, world!")
+//                    Text("Hello, world!")
+//                    Text("Hello, world!")
+//                }
+//                
+//                
+//                Text("Hello, world!")
+//                
+//            }
+//            .navigationTitle("SwiftUI")
+//            .navigationBarTitleDisplayMode(.inline)
+//        }
+//        
+//    }
+    /// Swift UI wants to know how to read & write to the name var
+    @State private var name = ""
+    
+    
     var body: some View {
-        NavigationStack {
-            Form {
-                Section {
-                    Text("Hello, world!")
-                    Text("Hello, world!")
-                    Text("Hello, world!")
-                }
-                
-                Section {
-                    Text("Hello, world!")
-                    Text("Hello, world!")
-                    Text("Hello, world!")
-                }
-                
-                
-                Text("Hello, world!")
-                
-            }
-            .navigationTitle("SwiftUI")
-            .navigationBarTitleDisplayMode(.inline)
+        Form {
+            TextField("Enter you name sir",text: $name)
+            /// $name creates a two-way binding so that you can update the text and display the updated text
+            Text("Your name is: \(name)") /// Just writing name means I want to just read the name var
         }
-        
     }
 }
 
