842bb04d1a0e4e0a3d5522b760059e215d3de276
Trust-Worthy <jondeveloper0@gmail.com>
2025-01-15 20:13:34 -0500

    Learned about @state which checks a variable for any changes. Also got a refresher on two way bindings.

--- Project-1-WeSplit/WeSplit/WeSplit/ContentView.swift
+++ Project-1-WeSplit/WeSplit/WeSplit/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     /// Day 17 Code
-    @State private var checkAmount: Double = 0.0
+    @State private var checkAmount: Double = 0.0 // @State automatically watches for changes
     @State private var numberOfPeople: Int =  2
     @State private var tipPercentage: Int = 20
     
@@ -18,7 +18,19 @@ struct ContentView: View {
     
     
     var body: some View {
-        Text("Hello, Mom & Dad!")
+        Form {
+            Section {
+                // $checkAmount is the two way binding
+                // Amount shows when the user doesn't have anything entered
+                TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                // Locale is a huge struct that has all the users region settings
+            }
+            
+            Section  {
+                Text(checkAmount,format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+            }
+            
+        }
         
     }
 }
