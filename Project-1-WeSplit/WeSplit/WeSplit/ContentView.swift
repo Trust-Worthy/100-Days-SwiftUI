da4811e65124b2d82590bf1c49782420da38d151
Trust-Worthy <jondeveloper0@gmail.com>
2025-01-15 19:51:41 -0500

    Day 17 --> starting to code the UI for the WeSplit app.

--- Project-1-WeSplit/WeSplit/WeSplit/ContentView.swift
+++ Project-1-WeSplit/WeSplit/WeSplit/ContentView.swift
@@ -8,7 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
-
+    /// Day 17 Code
+    @State private var checkAmount: Double = 0.0
+    @State private var numberOfPeople: Int =  2
+    @State private var tipPercentage: Int = 20
+    
+    
+    let tipPercentages: [Int] = [10,15,20,25,0]
+    
+    
     var body: some View {
         Text("Hello, Mom & Dad!")
         
