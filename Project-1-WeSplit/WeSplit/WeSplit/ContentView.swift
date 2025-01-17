2074b3f343b2e42b294227afc0541f2628291411
Trust-Worthy <jondeveloper0@gmail.com>
2025-01-16 21:02:05 -0500

    Learned how to create a navigation link and how to create a navigation title using a Navigation stack on the outside of my Form.

--- Project-1-WeSplit/WeSplit/WeSplit/ContentView.swift
+++ Project-1-WeSplit/WeSplit/WeSplit/ContentView.swift
@@ -18,18 +18,33 @@ struct ContentView: View {
     
     
     var body: some View { /// When a state changes for a variable it will re-invoke the body property of the view
-        Form {
-            Section {
-                // $checkAmount is the two way binding
-                // Amount shows when the user doesn't have anything entered
-                TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
-                // Locale is a huge struct that has all the users region settings
-                    .keyboardType(.decimalPad) // make the keyboard more convienient for the user when typing in checks.
+        
+        NavigationStack {
+            Form {
+                Section {
+                    // $checkAmount is the two way binding
+                    // Amount shows when the user doesn't have anything entered
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    // Locale is a huge struct that has all the users region settings
+                        .keyboardType(.decimalPad) // make the keyboard more convienient for the user when typing in checks.
+                    
+                    Picker("Number of people",selection: $numberOfPeople) {
+                        ForEach(2..<100){
+                            Text("\($0) People")
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                    /// Declarative User Interface design
+                    /// Saying what we want instead of how to make every individual thing
+                }
+                
+                Section  {
+                    Text(checkAmount,format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+                
             }
-            
-            Section  {
-                Text(checkAmount,format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
-            }
+            .navigationTitle("WeSplit")
+            /// Views are able to show many inside them
             
         }
         
