610022aac1b202df6b3d9095ae875b6179aede48
Trust-Worthy <jondeveloper0@gmail.com>
2025-01-23 21:23:52 -0500

    Learned about vibrancy and how to add that sexy iOS feel

--- Project-2-actually/Guess-Flag/Guess-Flag/ContentView.swift
+++ Project-2-actually/Guess-Flag/Guess-Flag/ContentView.swift
@@ -13,12 +13,23 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
+            // ** Things at the top of the Z stack are written first.
+            // ** Then things below are written on top
+            VStack(spacing: 0){
+                Color.red
+                Color.blue
+            }
             //Color.primary // flips colors --> semantic colors
             //Color.indigo // This is a view in its own right
-            Color(red:234,green: 0.8, blue:123) // Make your own color
+            //Color(red:234,green: 0.8, blue:123) // Make your own color
                 //.frame(width: 200,height:200)
                 //.frame(minWidth: 200, maxWidth: .infinity, maxHeight: 200) // This makes it flexible
             Text("Your content")
+                .foregroundStyle(.secondary)
+                .padding(50)
+                .background(.ultraThinMaterial) // Frosted glass effect
+            // iOS called vibrancy. Makes text standout on any background!
+            
         }
         //.background(.blue) Just the background of the Text object
         .ignoresSafeArea() // all important context should be in the safe area!!
