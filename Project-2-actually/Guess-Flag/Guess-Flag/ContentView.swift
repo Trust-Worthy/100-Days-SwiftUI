464201dec477deaeb4a3af65070c1c7fea7618a3
Trust-Worthy <jondeveloper0@gmail.com>
2025-01-23 21:16:15 -0500

    Learning about safe areas and colors.

--- Project-2-actually/Guess-Flag/Guess-Flag/ContentView.swift
+++ Project-2-actually/Guess-Flag/Guess-Flag/ContentView.swift
@@ -2,15 +2,27 @@
 //  ContentView.swift
 //  Guess-Flag
 //
-//  Created by Jonathan Bateman on 1/23/25.
+//  Created by Trust-Worthy on 1/23/25.
 //
+
+// The colors span the width of the phone, but it depends on the model.
+
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-       Text("Hello World!")
-        Text("This is another text view")
+        ZStack {
+            //Color.primary // flips colors --> semantic colors
+            //Color.indigo // This is a view in its own right
+            Color(red:234,green: 0.8, blue:123) // Make your own color
+                //.frame(width: 200,height:200)
+                //.frame(minWidth: 200, maxWidth: .infinity, maxHeight: 200) // This makes it flexible
+            Text("Your content")
+        }
+        //.background(.blue) Just the background of the Text object
+        .ignoresSafeArea() // all important context should be in the safe area!!
+        
     }
 }
 
