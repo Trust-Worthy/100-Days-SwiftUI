76e5f82a9f80e96133327c884b596a27b9564b84
Trust-Worthy <jondeveloper0@gmail.com>
2025-01-23 21:01:24 -0500

    This is actually project 2. The other day was a challenge project.

---
+++ Project-2-actually/Guess-Flag/Guess-Flag/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  Guess-Flag
+//
+//  Created by Jonathan Bateman on 1/23/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+       Text("Hello World!")
+        Text("This is another text view")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Project-2-actually/Guess-Flag/Guess-Flag/Guess_FlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_FlagApp.swift
+//  Guess-Flag
+//
+//  Created by Jonathan Bateman on 1/23/25.
+//
+
+import SwiftUI
+
+@main
+struct Guess_FlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
