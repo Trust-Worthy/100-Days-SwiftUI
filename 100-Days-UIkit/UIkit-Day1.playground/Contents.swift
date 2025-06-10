5953dc0997d6088de8d3493e27162558c2da7fd9
Trust-Worthy <jondeveloper0@gmail.com>
2025-06-10 10:32:54 -0600

    Done with day 1.

--- 100-Days-UIkit/UIkit-Day1.playground/Contents.swift
+++ 100-Days-UIkit/UIkit-Day1.playground/Contents.swift
@@ -1,3 +1,27 @@
 import UIKit
 
 var greeting: String = "Hello, playground"
+
+
+var age: Int = 38
+var population: Int = 8_000_000
+
+var str1 = """
+This is a multi-line string. \
+This is dope. \
+Hiii
+"""
+
+var pi: Double = 3.141
+
+var awersome: Bool = true
+
+// String interpolation
+
+var score: Int = 45
+var show_score: String = "Your score was \(score)"
+
+let taylor: String = "swift"
+
+// type annotations
+let album: String = "Reputation"
