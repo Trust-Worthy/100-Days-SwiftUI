0706acb61cc9ab278ac70856a1f9353f237fd192
Trust-Worthy <jondeveloper0@gmail.com>
2025-06-10 10:41:07 -0600

    I don't need to review all the syntax. I'm just going to go to the parts that make sense.

--- 100-Days-UIkit/UIkit-Day2.playground/Contents.swift
+++ 100-Days-UIkit/UIkit-Day2.playground/Contents.swift
@@ -1,3 +1,15 @@
 import UIKit
 
-var greeting = "Hello, playground"
+
+let john: String = "john"
+let jim: String = "jim"
+let bob: String = "bob"
+
+let friends: [String] = [john,jim,bob]
+
+let colors: Set<String> = Set(["red","green","blue"])
+
+
+let first_tuple = (first:"Taylor",last:"Swift")
+first_tuple.first
+first_tuple.last
