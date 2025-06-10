961b62d6b709468d71a46af9a5285cdd5f1c3692
Trust-Worthy <jondeveloper0@gmail.com>
2025-06-10 13:44:39 -0600

    Just learned how to throw from a function again. A nice little refresher.

---
+++ 100-Days-UIkit/UIKit-bundle-syntax-review.playground/Contents.swift
@@ -0,0 +1,69 @@
+import UIKit
+
+
+
+let card: Int = 9
+let card2: Int = 2
+
+if card > card2 {
+    print("Card 1 is larger than card 2")
+}
+
+// functions
+
+func printHelp() {
+    let message: String = "help help help SOS"
+    
+    print(message)
+}
+
+printHelp()
+
+func square(num number: Int) {
+    print(number * number)
+}
+square(num: 33)
+
+func squareRet(num number: Int) -> Int {
+    return number * number
+}
+
+func sayHello(to name: String) {
+    print("Hello, \(name)!")
+}
+
+sayHello(to: "jom")
+
+func greet(_ person: String, nicely: Bool = true) {
+    if nicely {
+        print("Kind hello to you \(person)")
+    }
+    else {
+        print("UZ a mean person \(person)")
+    }
+}
+
+greet("Jimmy", nicely: false)
+
+// Variadic function
+
+func squareMany(numbers: Int...) {
+    for num in numbers {
+        print("\(num) squared is \(num * num)")
+    }
+}
+
+squareMany(numbers: 1,2,3,4,5)
+
+// throwing functions
+
+enum PasswordError: Error {
+    case obvious
+}
+
+func checkPassword(_ password: String) throws -> Bool{
+    if password == "password" {
+        throw PasswordError.obvious
+    }
+    return true
+}
