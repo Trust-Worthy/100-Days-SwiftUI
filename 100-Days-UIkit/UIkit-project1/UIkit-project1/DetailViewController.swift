6729b0e674b73a191e2fa99576dc8b4975c8bcbd
Trust-Worthy <jondeveloper0@gmail.com>
2025-07-15 17:44:49 -0600

    Done with the project. I was able to change the title to something smaller.

--- 100-Days-UIkit/UIkit-project1/UIkit-project1/DetailViewController.swift
+++ 100-Days-UIkit/UIkit-project1/UIkit-project1/DetailViewController.swift
@@ -16,11 +16,25 @@ class DetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        title = selectedImage
+        navigationItem.largeTitleDisplayMode = .never // making it small on just this screen
+        
         if let imageToLoad = selectedImage {
             imageView.image = UIImage(named: imageToLoad)
         }
     }
     
+    // hide the bar when the image loads
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+        
+    }
+    // show the bar when the image is going away
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
 
 }
--- 100-Days-UIkit/UIkit-project1/UIkit-project1/ViewController.swift
+++ 100-Days-UIkit/UIkit-project1/UIkit-project1/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        title = "Storm Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
         
         
         // Get access to files
