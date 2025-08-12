20192abe8bed1208ac88ccab8629c6b952d24ee1
Trust-Worthy <jondeveloper0@gmail.com>
2025-08-12 10:44:17 -0600

    Added constraints for image view. Also created new view controller for the image view.

---
+++ 100-Days-UIkit/storm-viewer/storm-viewer/DetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DetailViewController.swift
+//  storm-viewer
+//
+//  Created by Jonathan Bateman on 8/12/25.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
