76506f50bdae9f0fe8ae51475794fa041e77e179
Trust-Worthy <jondeveloper0@gmail.com>
2025-08-12 11:58:25 -0600

    Added the button to the view controller file.

--- 100-Days-UIkit/guess-the-flag-uikit-proj/guess-the-flag-uikit-proj/ViewController.swift
+++ 100-Days-UIkit/guess-the-flag-uikit-proj/guess-the-flag-uikit-proj/ViewController.swift
@@ -2,13 +2,17 @@
 //  ViewController.swift
 //  guess-the-flag-uikit-proj
 //
-//  Created by Jonathan Bateman on 8/12/25.
+//  Created by Trust-Worthy on 8/12/25.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
