27bb386186fb759451cc5c141aaa125ca5149f6b
Trust-Worthy <jondeveloper0@gmail.com>
2025-08-12 10:06:40 -0600

    Just learned how to use FileManager in swift

--- 100-Days-UIkit/storm-viewer/storm-viewer/ViewController.swift
+++ 100-Days-UIkit/storm-viewer/storm-viewer/ViewController.swift
@@ -8,23 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var pictures = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
-        let fm = FileManager.default
+        let fm = FileManager.default // built in system type for working with files
         
-        let path = Bundle.main.resourcePath!
+        let path = Bundle.main.resourcePath! // sets resource path
+        // bundle --> directory containing code for app
         
         let items = try! fm.contentsOfDirectory(atPath: path)
         
         for item in items {
-            if item.description.hasPrefix("nssl") {
+            if item.hasPrefix("nssl") {
                 // this is a picture to load
+                // append the file name to the list
+                pictures.append(item)
                 
             }
         }
+        
+        print(pictures)
     }
 
 
