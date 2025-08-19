13191ab620f55cb10c92eaf40a9257ead1b50197
Trust-Worthy <jondeveloper0@gmail.com>
2025-08-18 19:32:48 -0700

    Added the progress view.

--- project4/project4/ViewController.swift
+++ project4/project4/ViewController.swift
@@ -11,6 +11,8 @@ import WebKit
 // extends from UIViewController and conforms to WKNav
 class ViewController: UIViewController, WKNavigationDelegate{
     var webView: WKWebView!
+    var progressView: UIProgressView!
+    
     
     override func loadView() {
         
@@ -26,6 +28,21 @@ class ViewController: UIViewController, WKNavigationDelegate{
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
         
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        // reloads the webview in place
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+        
+        
+        progressView = UIProgressView(progressViewStyle: .default)
+        progressView.sizeToFit() // take up as much space as it needs
+        let progressButton = UIBarButtonItem(customView: progressView)
+        
+        toolbarItems = [progressButton, spacer,refresh] // ui view controller tool bar array
+        navigationController?.isToolbarHidden = false
+        
+        // make sure to remove observer when working in complex apps
+        // must implement
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
         
         let url = URL(string: "https://www.hackingwithswift.com")!
         webView.load(URLRequest(url: url)) // turns url into url request
@@ -36,7 +53,7 @@ class ViewController: UIViewController, WKNavigationDelegate{
     @objc func openTapped() {
         let ac = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
         ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
-        ac.addAction(UIAlertAction(title: "hackingwithswift.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "404media.co", style: .default, handler: openPage))
         ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         
         ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem // ipad code
@@ -61,6 +78,14 @@ class ViewController: UIViewController, WKNavigationDelegate{
         
         // view controllers title
         title = webView.title
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        
+        if keyPath == "estimatedProgress" {
+            
+            progressView.progress = Float(webView.estimatedProgress)
+        }
     }
 
 }
