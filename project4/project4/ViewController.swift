e1239207ebad315db2200b71803c7b914d94dbcb
Trust-Worthy <jondeveloper0@gmail.com>
2025-08-18 19:04:05 -0700

    Added UIAlertController and actions so the user can choose what website opens.

--- project4/project4/ViewController.swift
+++ project4/project4/ViewController.swift
@@ -24,12 +24,44 @@ class ViewController: UIViewController, WKNavigationDelegate{
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        
         
         let url = URL(string: "https://www.hackingwithswift.com")!
         webView.load(URLRequest(url: url)) // turns url into url request
         webView.allowsBackForwardNavigationGestures = true
     }
 
+    
+    @objc func openTapped() {
+        let ac = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "hackingwithswift.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
+        ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem // ipad code
+        
+        present(ac, animated: true)
+    }
+    
+    // openPage is the handler when the user touches something
+    func openPage(action: UIAlertAction) {
+        
+        // super safe code
+        guard let actionTitle = action.title else {return}
+        guard let url = URL(string: "https://" + actionTitle ) else {return} // double force unwrap
+        
+        webView.load(URLRequest(url: url))
+        
+        
+    }
+    
+    // called when web page has finished loading its page
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        
+        // view controllers title
+        title = webView.title
+    }
 
 }
 
