daac4c5d810e9e9171fc23a18ac837af3eb010b8
Trust-Worthy <jondeveloper0@gmail.com>
2025-08-18 18:37:01 -0700

    Working on project 4 aka day 24 of uikit tutorial

---
+++ project4/project4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project4
+//
+//  Created by Jonathan Bateman on 8/18/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
