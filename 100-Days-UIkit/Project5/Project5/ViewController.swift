216b6f9be8be4ca36a38dbff47244a663f0cea4e
Trust-Worthy <jondeveloper0@gmail.com>
2025-09-06 12:06:48 -0600

    Added start.txt.

--- 100-Days-UIkit/Project5/Project5/ViewController.swift
+++ 100-Days-UIkit/Project5/Project5/ViewController.swift
@@ -2,18 +2,22 @@
 //  ViewController.swift
 //  Project5
 //
-//  Created by Jonathan Bateman on 9/6/25.
+//  Created by Trust-Worthy on 9/6/25.
 //
 
 import UIKit
 
 class ViewController: UITableViewController {
+    
+    var allWords = [String]()
+    var usedWords = [String]()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
 
 }
 
