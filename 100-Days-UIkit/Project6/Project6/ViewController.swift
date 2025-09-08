92bd2ae10b98c50fb6318a818bf810946fce2b09
Trust-Worthy <jondeveloper0@gmail.com>
2025-09-08 16:06:05 -0600

    Creating first labels in code.

---
+++ 100-Days-UIkit/Project6/Project6/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  Project6
+//
+//  Created by Trust-Worthy on 9/8/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let label1 = UILabel()
+        label1.translatesAutoresizingMaskIntoConstraints = false
+        label1.backgroundColor = .red
+        label1.text = "THESE"
+        label1.sizeToFit()
+        
+        let label2 = UILabel()
+        label2.translatesAutoresizingMaskIntoConstraints = false
+        label2.backgroundColor = .red
+        label2.text = "THESE"
+        label2.sizeToFit()
+        
+        let label3 = UILabel()
+        label3.translatesAutoresizingMaskIntoConstraints = false
+        label3.backgroundColor = .red
+        label3.text = "THESE"
+        label3.sizeToFit()
+        
+        let label4 = UILabel()
+        label4.translatesAutoresizingMaskIntoConstraints = false
+        label4.backgroundColor = .red
+        label4.text = "THESE"
+        label4.sizeToFit()
+        
+        let label5 = UILabel()
+        label5.translatesAutoresizingMaskIntoConstraints = false
+        label5.backgroundColor = .red
+        label5.text = "THESE"
+        label5.sizeToFit()
+        
+    }
+
+
+}
+
