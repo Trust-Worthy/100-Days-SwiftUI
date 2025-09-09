eb53b58ebae4bda18733f068cb39b748451dfe45
Trust-Worthy <jondeveloper0@gmail.com>
2025-09-09 08:54:37 -0600

    Finished the table view methods.

--- 100-Days-UIkit/day32-challengeproject/day32-challengeproject/ViewController.swift
+++ 100-Days-UIkit/day32-challengeproject/day32-challengeproject/ViewController.swift
@@ -9,19 +9,48 @@ import UIKit
 
 class ViewController: UITableViewController {
 
-    var shoppingList: [String]?
+    var shoppingList = [String]()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        title = "Shopping List"
+        
         navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(clearShoppingList))
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addShoppingListItem))
+        
+        
+    }
+    
+    // MARK: - Table View Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return shoppingList.count
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Item", for: indexPath)
+        
+        cell.textLabel?.text = shoppingList[indexPath.row]
+        
+        return cell
+    }
+    
+    @objc private func addShoppingListItem() {
         
         
         
     }
     
-    @objc private func clearShoppingList
+    @objc private func clearShoppingList() {
+        
+    }
+    
+   
 
 
 }
