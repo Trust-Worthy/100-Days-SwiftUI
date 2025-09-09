ba7b4a564b1ccd429dc52a95c5568da722a8eb16
Trust-Worthy <jondeveloper0@gmail.com>
2025-09-09 09:31:08 -0600

    Setup the basic view controller with the tableview.

---
+++ 100-Days-UIkit/Project7/Project7/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Project7
+//
+//  Created by Jonathan Bateman on 9/9/25.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    var petitions = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return petitions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = "Title goes here"
+        cell.detailTextLabel?.text = "Subtitle goes here"
+        
+        return cell
+    }
+
+
+}
+
