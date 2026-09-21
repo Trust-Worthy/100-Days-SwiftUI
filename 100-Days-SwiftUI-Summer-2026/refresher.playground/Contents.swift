90a5a7f85e1c42bb8caa1631320888cdf07f92c9
Trust-Worthy <jondeveloper0@gmail.com>
2026-09-21 18:47:00 -0400

    Thank the Lord that I'm on day 13

--- 100-Days-SwiftUI-Summer-2026/refresher.playground/Contents.swift
+++ 100-Days-SwiftUI-Summer-2026/refresher.playground/Contents.swift
@@ -1,32 +1,58 @@
 import Cocoa
 
-struct School {
-    static var studentCount = 0
+protocol Vehicle {
+    var name: String { get }
+    var currentPassengers: Int { get set }
+    func estimateTime(for distance: Int) -> Int
+    func travel(distance: Int)
+}
+struct Car: Vehicle {
+    let name: String = "Car"
+    var currentPassengers = 1
     
-    static func add(student: String) {
-        print("\(student) joined the school")
-        studentCount += 1
+    func estimateTime(for distance: Int) -> Int {
+        distance / 50
+    }
+    
+    func travel(distance: Int) {
+        print("I'm driving \(distance)km")
+    }
+    
+    func openSunroof() {
+        print("It's a nice day!")
+    }
+    
+}
+
+struct Bicycle: Vehicle {
+    let name: String = "Bicycle"
+    
+    var currentPassengers: Int = 1
+    
+    func estimateTime(for distance: Int) -> Int {
+        distance / 10
+    }
+    
+    func travel(distance: Int) {
+        print("I'm cycling \(distance)km")
+    }
+}
+
+func commute(distance: Int, using vehicle: Vehicle) {
+    
+    if vehicle.estimateTime(for: distance) > 100 {
+        print("That's too slow cuz! I'll try a different vehicle.")
+    } else {
+        vehicle.travel(distance: distance)
+    }
+}
+
+func getTravelEstimates(using vehicles: [Vehicle], distance: Int) {
+    
+    for vehicle in vehicles {
+        let estimate = vehicle.estimateTime(for: distance)
+        print("\(vehicle.name): \(estimate) house to travel \(distance)km")
     }
 }
 
 
-School.add(student: "Biggy Smalls")
-print(School.studentCount)
-
-
-struct AppData {
-    static let version =  "1.3 beta 2"
-    static let saveFilename = "settings.json"
-    static let homeURL = "https://www.realxreal.app"
-}
-
-
-struct Employee {
-    let username: String
-    let password: String
-    
-    static let example = Employee(username: "testtest", password: "fakepassword")
-}
-
-
-Employee.example
